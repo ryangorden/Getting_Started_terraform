8a4c9f07e61c2afa38dcdc46a1e76db21a927efd
ryangorden <63272391+ryangorden@users.noreply.github.com>
2023-01-17 21:15:50 -0600

    Update networking.tf

--- m3/networking.tf
+++ m3/networking.tf
@@ -5,37 +5,37 @@ name = "ps-rg"
 location = "eastus"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-name = "ps-vnet"
-address_space = ["10.0.0.0/16"]
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_virtual_network" "vnet" {
+# name = "ps-vnet"
+# address_space = ["10.0.0.0/16"]
+# location = azurerm_resource_group.rg.location
+# resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_subnet" "subnet1" {
-name = "web"
-resource_group_name = azurerm_resource_group.rg.name
-virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = ["10.0.0.0/24"]
-}
+# resource "azurerm_subnet" "subnet1" {
+# name = "web"
+# resource_group_name = azurerm_resource_group.rg.name
+# virtual_network_name = azurerm_virtual_network.vnet.name
+# address_prefixes = ["10.0.0.0/24"]
+# }
 
-resource "azurerm_public_ip" "pip" {
-name = "ps-pip"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-allocation_method = "Dynamic"
-}
+# resource "azurerm_public_ip" "pip" {
+# name = "ps-pip"
+# location = azurerm_resource_group.rg.location
+# resource_group_name = azurerm_resource_group.rg.name
+# allocation_method = "Dynamic"
+# }
 
-resource "azurerm_network_interface" "nic" {
-name = "ps-nic"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-ip_configuration {
-name = "example-config"
-subnet_id = azurerm_subnet.subnet1.id
-private_ip_address_allocation = "Dynamic"
-public_ip_address_id = azurerm_public_ip.pip.id
-}
-}
+# resource "azurerm_network_interface" "nic" {
+# name = "ps-nic"
+# location = azurerm_resource_group.rg.location
+# resource_group_name = azurerm_resource_group.rg.name
+# ip_configuration {
+# name = "example-config"
+# subnet_id = azurerm_subnet.subnet1.id
+# private_ip_address_allocation = "Dynamic"
+# public_ip_address_id = azurerm_public_ip.pip.id
+# }
+# }
 
 
