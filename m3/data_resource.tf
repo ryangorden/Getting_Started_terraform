cd914ea9d097a2296fab66013693cb779faf43c0
ryangorden <63272391+ryangorden@users.noreply.github.com>
2023-01-17 21:14:02 -0600

    Update data_resource.tf

--- m3/data_resource.tf
+++ m3/data_resource.tf
@@ -1,11 +1,11 @@
-##################################################################################
+# ##################################################################################
 
-# DATA
+# # DATA
 
-##################################################################################
+# ##################################################################################
 
-data "azurerm_image" "image" {
-name = "Canonical:UbuntuServer:19_04-daily-gen2:19.04.201911140"
-resource_group_name = azurerm_resource_group.rg.name
-depends_on = [azurerm_resource_group.rg]
-}
+# data "azurerm_image" "image" {
+# name = "Canonical:UbuntuServer:19_04-daily-gen2:19.04.201911140"
+# resource_group_name = azurerm_resource_group.rg.name
+# depends_on = [azurerm_resource_group.rg]
+# }
