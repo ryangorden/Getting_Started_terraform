b356939691b5df542eab01c1f7f469ac5bbc71a3
ryangorden <63272391+ryangorden@users.noreply.github.com>
2023-01-17 21:19:47 -0600

    Update vm.tf

--- m3/vm.tf
+++ m3/vm.tf
@@ -1,27 +1,27 @@
-# # INSTANCES
+# INSTANCES
 
-# resource "azurerm_virtual_machine" "nginx1" {
-# name = "nginx1"
-# location = azurerm_resource_group.rg.location
-# resource_group_name = azurerm_resource_group.rg.name
-# network_interface_ids = [azurerm_network_interface.nic.id]
-# vm_size = "Standard_DS1_v2"
-# storage_image_reference {
-# id = data.azurerm_image.image.id
-# }
-# storage_os_disk {
-# name = "nginx1-os-disk"
-# #vhd_uri = "${azurerm_storage_account.storage.primary_blob_endpoint}/nginx1-os-disk.vhd"
-# caching = "ReadWrite"
-# create_option = "FromImage"
-# managed_disk_type = "Standard_LRS"
-# }
-# os_profile {
-# computer_name = "nginx1"
-# admin_username = ""
-# admin_password = ""
-# }
-# os_profile_linux_config {
-# disable_password_authentication = false
-# }
-# }
+resource "azurerm_virtual_machine" "nginx1" {
+name = "nginx1"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+network_interface_ids = [azurerm_network_interface.nic.id]
+vm_size = "Standard_DS1_v2"
+storage_image_reference {
+id = data.azurerm_image.image.id
+}
+storage_os_disk {
+name = "nginx1-os-disk"
+#vhd_uri = "${azurerm_storage_account.storage.primary_blob_endpoint}/nginx1-os-disk.vhd"
+caching = "ReadWrite"
+create_option = "FromImage"
+managed_disk_type = "Standard_LRS"
+}
+os_profile {
+computer_name = "nginx1"
+admin_username = ""
+admin_password = ""
+}
+os_profile_linux_config {
+disable_password_authentication = false
+}
+}
